5e08b4760099ca26c304165e17d44f07158a5acd
Jacob Amrine <64110198+jacobamrine@users.noreply.github.com>
2026-05-10 23:11:50 -0600

    Initial testing

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
+  name: "Jacob Amrine",
   title: "Senior Software Engineer",
   description: "Portfolio website of Ryan Fitzgerald",
   accentColor: "#1d4ed8",
